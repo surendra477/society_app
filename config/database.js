e56a0f1f600e7a34abe8d7f6436fdcfc0d35924f
Surendra <58383796+surendra477@users.noreply.github.com>
2021-09-15 01:13:23 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
-
+const { MONGO_URI } = process.env;
 exports.connect = () => {
   // Connecting to the database
   mongoose
-    .connect("mongodb://127.0.0.1:27017/society_app", {
+    .connect(MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     
