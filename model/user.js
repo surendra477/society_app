64f8bf80c6e4f3672a1847c1360c44de9cee61db
Surendra <58383796+surendra477@users.noreply.github.com>
2021-09-15 01:25:05 +0530

    Update user.js

--- model/user.js
+++ model/user.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     name: { type: String, default: null },
     room_number: { type: String, default: null },
-    building_number: { type: String, default: null },
+    build_number: { type: String, default: null },
     password: { type: String },
     email:{ type: String,unique:true},
     token: { type: String },
