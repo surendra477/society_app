2e1f8421ef1e470c07afff7e3e2156f0c23bca83
Surendra <58383796+surendra477@users.noreply.github.com>
2021-09-16 12:47:18 +0530

    Update meeting.js

--- model/meeting.js
+++ model/meeting.js
@@ -2,8 +2,9 @@ const mongoose = require("mongoose");
 
 const MeetingSchema = new mongoose.Schema({
     title: { type: String, default: null },
+    time: { type: String, default: null },
     agenda:{type: String, default: null },
     description: { type: String, default: null },
 });
+module.exports = mongoose.model("meetings", userSchema);
 
-module.exports = mongoose.model("meeting", MeetingSchema);
