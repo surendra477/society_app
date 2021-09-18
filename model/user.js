88f1161fae9039b0ce0e0ea4ae5ffae42a8c9717
Surendra <58383796+surendra477@users.noreply.github.com>
2021-09-18 09:36:29 +0530

    Update user.js

--- model/user.js
+++ model/user.js
@@ -5,7 +5,13 @@ const userSchema = new mongoose.Schema({
     room_number: { type: String, default: null },
     building_number: { type: String, default: null },
     password: { type: String },
-    email:{ type: String,unique:true
+    email:{ type: String,unique:true,
+            trim: true,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+        validate: [validateEmail, 'Please fill a valid email address'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
          },
     token: { type: String },
 });
