7cd4b501807457b8a60344ae11a2154a5226e0df
Surendra <58383796+surendra477@users.noreply.github.com>
2021-09-21 13:16:33 +0530

    Update notice.js

--- model/notice.js
+++ model/notice.js
@@ -1,1 +1,10 @@
 
+const mongoose = require("mongoose");
+
+const NoticeSchema = new mongoose.Schema({
+  
+    title:{type: String, default: null },
+    description: { type: String, default: null },
+});
+
+module.exports = mongoose.model("notice",NoticeSchema);
