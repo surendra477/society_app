0851a6333373421dbf7c8b63a8e0311bbb6ae937
Surendra <58383796+surendra477@users.noreply.github.com>
2021-09-22 21:27:27 +0530

    Add files via upload

--- model/complaint.js
+++ model/complaint.js
@@ -1,1 +1,12 @@
 
+const mongoose = require("mongoose");
+
+const ComplaintsSchema = new mongoose.Schema({
+    //name: { type: String, default: null },
+   // time: { type: String, default: null },
+    title:{type: String, default: null },
+    description: { type: String, default: null },
+    image: { type: String, default: null },
+});
+
+module.exports = mongoose.model("complaint", ComplaintsSchema);
